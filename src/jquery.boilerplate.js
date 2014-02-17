8c5b0ba51203d4a6b68a5d9522ea10e7a04535eb
RENAUDIN Yann <renaudin.yann@gmail.com>
2014-02-18 00:27:03 +0100

    Clean Repository off useless boilerplate

--- src/jquery.boilerplate.js
+++ src/jquery.boilerplate.js
@@ -12,7 +12,7 @@
 		// minified (especially when both are regularly referenced in your plugin).
 
 		// Create the defaults once
-		var pluginName = "defaultPluginName",
+		var pluginName = "faPicker",
 				defaults = {
 				propertyName: "value"
 		};
