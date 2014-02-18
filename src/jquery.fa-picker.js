5786878a738aecf929d6ab4661cc6f84495397aa
RENAUDIN Yann <renaudin.yann@gmail.com>
2014-02-18 16:12:11 +0100

    Singleton on icons data loading Readme updated

--- src/jquery.fa-picker.js
+++ src/jquery.fa-picker.js
@@ -17,19 +17,30 @@
         this.element = element;
         this.settings = $.extend({}, defaults, options);
         this._defaults = defaults;
+        this._iconsList=[];
         this._name = pluginName;
         this.init();
     }
 
     Plugin.prototype = {
         init: function() {
-            this.bootstrapIt();
+            this.bootstrapIt(); // bootstrap element on which plugin has been activated
             this.bindListeners();
         },
-        loadIcons: function() {
-            return $.getJSON(this.settings.iconsFile).then(function(data) {
-                return data;
-            });
+        getListIcons: function() {
+    		var deferred = new jQuery.Deferred();
+        	var _self=this;
+        	// Do some kind of singleton to load json data once
+           	if(_self._iconsList.length == 0){
+				return  $.getJSON(_self.settings.iconsFile).then(function(data) {  
+					_self._iconsList = data.icons;           	
+                	return _self._iconsList;
+            	}); 
+			}
+			else{
+				deferred.resolve(_self._iconsList);
+				return deferred.promise();
+			}
 
         },
         bindListeners: function() {
@@ -48,14 +59,14 @@
             if (event.relatedTarget === _self.element) { // Check wether the event is aimed to this element
             	
                	var $iconsContainer= $(event.target).find(".modal-body").append("<div class='fa-container'></div>");
-           		/*_self.loadIcons()
-           			.done(function(data){
-           				
-           				$.each(data.icons,function(index,icon){
-           					console.log($iconsContainer);
-           					$iconsContainer.append("<div class='item'><i class='fa fa-"+icon.id+" fa-2x'></i></div>");
+           		_self.getListIcons()
+           			.done(function(listIcons){
+           				console.log(listIcons.length);
+           				$.each(listIcons,function(index,icon){
+           					//console.log($iconsContainer);
+           					//$iconsContainer.append("<div class='item'><i class='fa fa-"+icon.id+" fa-2x'></i></div>");
            				});
-           			});*/
+           			});
             }
 
         }
